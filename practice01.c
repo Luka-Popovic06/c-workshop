7339863032a085479c1d5b9cc530c5abfba4922a
Luka Popovic <lukapopovic0111@gmail.com>
2025-11-18 13:48:20 +0100

    Add task02 function

--- practice01.c
+++ practice01.c
@@ -36,7 +36,28 @@ void task01(){ //void znači da funkcija ne vraća nikakvu vrednost
   
 }
 
+
+//TASK-2
+
+void task02(){
+  //Kreiraju se dve promenljive tipa float (brojevi sa decimalama).
+  //Primer vrednosti: 3.5, 7.2, -1.0.
+  //double - mozemo i ovo da koristimo za brojeve sa decimalama (64 bit)
+  //float (32 bit)
+  float a;
+  float b;
+
+  printf("Unesite prvi broj:\n");
+  scanf("%f", &a);
+  printf("Unesite drugi broj: \n");
+  scanf("%f", &b);
+
+  printf("Zbir: %f \n", a+b);
+  printf("Razlika: %f \n", a-b);
+
+}
+
 int main(){
-  task01();
+  task02();
   return 0;
 }
