6b5df293c19129bf77b01e8b4cfd9b68a65b3663
Luka Popovic <lukapopovic0111@gmail.com>
2025-11-18 13:26:42 +0100

    Add task01 function to perform basic arithmetic operations

--- practice01.c
+++ practice01.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* 
 int main(){
   printf("Hello world!\n");
+  return 0;
+} 
+*/
+
+
+//TASK-1
+
+void task01(){ //void znači da funkcija ne vraća nikakvu vrednost
+  int a;//promenljiva / tipa int (celobrojni tip) / integer
+  int b;
+
+  // printf ispisuje poruku na ekran i traži od korisnika unos.
+  // \n - novi red
+  printf("Unesite prvi broj:\n");
+
+  // scanf("%d", &a) čita celobrojni unos (%d) i smešta ga u promenljivu a.
+  // &a znači da šaljemo adresu promenljive da scanf može da upiše vrednost direktno u a.
+  scanf("%d", &a);
+
+  printf("Unesite drugi broj:\n");
+  scanf("%d", &b);
+
+  //%d je placeholder za celobrojni rezultat.
+  printf("Rezultat sabiranja je: %d \n", a+b);
+  printf("Rezultata oduzimanja je: %d \n", a-b);
+  printf("Rezultat množenja je: %d \n", a*b);
+  printf("Rezultat deljenja je: %d \n", a/b);
+
+  printf("Sabiranje: %d \n Oduzimanje: %d \n Množenje: %d \n ", a+b, a-b, a*b);
+  
+}
+
+int main(){
+  task01();
   return 0;
 }
