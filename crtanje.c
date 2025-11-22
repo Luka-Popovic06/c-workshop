717fa987705c23239e1d597e2d37b19a9e432c9e
Luka Popovic <lukapopovic0111@gmail.com>
2025-11-22 21:51:27 +0100

    Add task37 function

--- crtanje.c
+++ crtanje.c
@@ -11,7 +11,7 @@
 //(1,0) (1,1) (1,2) (1,3)
 //(2,0) (2,1) (2,2) (2,3)
 //(3,0) (3,1) (3,2) (3,3)
-//1.task
+//TASK-28
 void task28(int n){
   for(int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
@@ -21,7 +21,7 @@ void task28(int n){
   }
 }
 
-//2.task
+//TASK-29
 void task29(int n){
   for(int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
@@ -35,7 +35,7 @@ void task29(int n){
   }
 }
 
-//3.task
+//TASK-30
 void task30(int n){
   for(int i = 0; i < n; i++){
     for(int j = 0; j < n; j++){
@@ -49,7 +49,7 @@ void task30(int n){
   }
 }
 
-//4.task
+//TASK-31
 void task31(int n){
   for(int i = 0; i < n; i++){
     for(int j = 0; j < n; j++){
@@ -62,7 +62,7 @@ void task31(int n){
     printf("\n");
   }
 }
-//5.task
+//TASK-32
 void task32(int n){
   for (int r = 0; r < n; r++)
   {
@@ -79,7 +79,7 @@ void task32(int n){
   
 }
 
-//6.task
+//TASK-33
 void task33(int n){
   for (int i = 0; i < n; i++)
   {
@@ -90,7 +90,7 @@ void task33(int n){
   }
 }
 
-//7.task
+//TASK-34
 //Ugnjezdena petlja
 //Ispisati sve trocifrene brojeve cije su sve cifre razlicite.
 void task34(){
@@ -105,7 +105,7 @@ void task34(){
   }
 }
 
-//8.task
+//TASK-35
 /*Naci i ispisati sve savrsene brojeve u intervalu [1, n]. Savrseni broj je onaj tiji je zbir
 svih pravih delilaca jednak samom broju, npr. 6 = 1 + 2 + 3.*/
 void task35(int n){
@@ -120,7 +120,7 @@ void task35(int n){
   }
 }
 
-//9.task
+//TASK-36
 void task36(){
   // n! - faktorijal = n * (n-1) * (n-2)*... * 3 * 2 * 1
   //i! = 1 * 2*... * i
@@ -146,7 +146,33 @@ void task36(){
   printf("Suma reda je %d", s);
 }
 
+//TASK-37
+void task37(int a, int b){
+  int rezultat=0;
+  int faktor=1;
+  int tmp_a=a;
+  while(tmp_a > 0){
+    int cifra_a = tmp_a % 10;
+    int tmp_b = b;
+    int postoji = 0;//false
+    while (tmp_b > 0) {
+      int cifra_b=tmp_b%10;
+      if(cifra_a == cifra_b){
+        postoji = 1;//true
+        break;
+      }
+      tmp_b /= 10;
+    }
+    if(!postoji){
+      rezultat += cifra_a * faktor;
+      faktor *= 10;
+    }
+    tmp_a /=10;
+  }
+  printf("Rezultat je %d\n", rezultat);
+}
+
 int main(){
-  task36();
+  task37(1234567,467);
   return 0;
 }
