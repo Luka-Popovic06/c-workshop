f722aa2058fce5bca25ebda39e4b2d76785666e9
Luka Popovic <lukapopovic0111@gmail.com>
2025-11-22 21:20:01 +0100

    Add task29 function

--- crtanje.c
+++ crtanje.c
@@ -20,3 +20,18 @@ void task28(int n){
     printf("\n");
   }
 }
+
+//2.task
+void task29(int n){
+  for(int i = 0; i < n; i++){
+    for (int j = 0; j < n; j++){
+      if(i == 0 || i == n-1 || j == 0 || j == n - 1){
+        printf("*");
+      }else{
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
+
