1d2713ea51cb69f2b0f40b7a0ec0d9f9503bbec5
Luka Popovic <lukapopovic0111@gmail.com>
2025-12-24 21:17:55 +0100

    Add prime number check function

--- d.c
+++ d.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <math.h>
 
+
+// 1.
 //Faktorijal broja n (piše se n!) je proizvod svih celih brojeva od 1 do n.
 //n!=1⋅2⋅3⋅...⋅n
 
@@ -17,7 +19,31 @@ void calculateAndPrintFactorial(){
   }
   printf("Faktorijal broja %d je %d", br, rezultat);
 }
-int main(){
-  calculateAndPrintFactorial();
-  return 0;
+
+/*
+2.Ispitujemo da li je neki broj prost
+Prost broj je broj veći od 1 koji ima tačno dva delitelja: 1 i sebe samog.
+*/
+
+void isPrime(){
+  printf("Unesi broj:\n");
+  int br;
+  scanf("%d",&br);
+  if(br==1){
+      printf("Broj je prost");
+      return;
+  }else if(br < 1){
+    printf("Broj nije prost");
+      return;
+  }
+  
+  for(int i=2; i < br; i++){
+    if(br % i == 0)
+    {
+      printf("Broj %d. nije prost\n", br);
+      return;
+    }
+  }
+  printf("Broj %d. je prost broj\n", br);
+  
 }
