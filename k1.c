1a976733455c37fe147f593e5698f8221f8fe757
Luka Popovic <lukapopovic0111@gmail.com>
2026-01-26 14:42:22 +0100

    Add functions for digit geometric mean and middle digit calculation

--- k1.c
+++ k1.c
@@ -7,6 +7,7 @@
 Random funkcija:
 1.Napisati program koji simulira bacanje šestostrane, dvanaestostrane i 
 dvadesetostrane "kockice" (poliedra).
+1 - je prvi prirodan broj
 */
 void task01(){
   time_t t;
@@ -19,38 +20,69 @@ void task01(){
   printf("%d %d %d \n", kocka_1, kocka_2, kocka_3);
 
 }
-void task02(){
-  int n;
-  printf("Unesite broj n:\n");
-  scanf("%d", &n);
-  int max = 0;
-  int res = 0;
-  for (int i = 0; i < n; i++)
+
+/*
+2025/26
+k1 - grupa 5
+2.
+*/
+double g_sredina(int x){
+  int n = 0;//brojač cifara
+  double proizvod = 1.0;
+
+  while (x > 0)
   {
-    int br = 0;
-    int counter = 0;
-    printf("Unesi te %d. broj\n", i);
-    scanf("%d", &br);
-    int br_copy = br;
-    while (counter <= 3)
-    {
-      int c = br % 10;
-      if(counter == 3)
-        max = max < c ? c : max;
-
-      if(max < c)
-       res = br_copy;
-        
-
-      counter++;
-      br/=10;
-    }
-    
+    proizvod *= x % 10;
+    n++;
+    x /= 10;
   }
-  printf("%d \n", res);
+
+  return pow(proizvod, 1.0 / n);//1.0/n pravi realan stepen
+  //(koren na n)√ a1 · · · · · an.
+  //koren na nesto je ustvari (br / 1.0), i sa ovim kvadrirano
+  // treći koren od 7 je: pow(7, 1.0 / 3);
+}
+int srednja_cifra(int n){
+  int copy_n = n;
+  int br_cifara = 0;
+  int redni_br_srednje_cif = 0;
+  int c = 0;
+  while (n > 0)
+  {
+    br_cifara++;
+    n/=10;
+  }
+  if(br_cifara % 2 != 0 ){
+    redni_br_srednje_cif = br_cifara / 2 + 1;
+    while (redni_br_srednje_cif > 0)
+    {
+      
+      if(redni_br_srednje_cif == 1){
+        c = copy_n % 10;
+      }
+       copy_n /= 10;
+      redni_br_srednje_cif--;
+    }
+  }else{
+    redni_br_srednje_cif = br_cifara / 2;
+    while (redni_br_srednje_cif >= 0)
+    { 
+      if(redni_br_srednje_cif == 1){
+        c = copy_n % 10;
+      }
+      if(redni_br_srednje_cif == 0){
+        c = c + ((copy_n % 10) * 10);
+      }
+      copy_n /= 10;
+      redni_br_srednje_cif--;
+    }
+  }
+
+  return c;
 }
 
+
 int main(){
-  task02();
+  printf("%d\n", uslov2(889)); //2345678
   return 0;
 }
